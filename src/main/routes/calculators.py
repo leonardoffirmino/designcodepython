9ca93346a8670889f3decb79712955aec1703591
Leonardo <firmino750@gmail.com>
2025-08-23 20:23:22 -0300

    Include rote in file

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,6 +1,8 @@
 from flask import Blueprint,jsonify,request
 from main.factories.calculator1_factory import calculator1_factory
 from main.factories.calculator2_factory import calculator2_factory
+from main.factories.calculator3_factory import calculator3_factory
+
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
@@ -14,5 +16,11 @@ def calculator_1():
 @calc_route_bp.route("/calculator/2",methods=["POST"])
 def calculator_2():
   calc = calculator2_factory()
+  response = calc.calculate(request)
+  return jsonify(response),200
+
+@calc_route_bp.route("/calculator/3",methods=["POST"])
+def calculator_3():
+  calc = calculator3_factory()
   response = calc.calculate(request)
   return jsonify(response),200

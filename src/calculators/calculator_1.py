e715e218d74234c9fe3d2fa8a6736dc321f653d7
Leonardo <firmino750@gmail.com>
2025-08-30 16:08:02 -0300

    Finisih project with processing error

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from flask import request as FlaskRequest
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
 
 
 class Calculator_1:
@@ -18,7 +19,7 @@ class Calculator_1:
   
   def __validate_body(self,body: Dict) -> float:
     if "number" not in body:
-      raise Exception("Body mal formatado!")
+      raise HttpUnprocessableEntityError("Body mal formatado!")
       
     input_data = body["number"]
     return input_data
--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,6 +1,9 @@
 from flask import request as FlaskRequest
 from typing import Dict, List
 from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
+from src.errors.http_bad_request import HttpBadRequestError
+
 
 
 class Calculator3:
@@ -21,7 +24,7 @@ class Calculator3:
 
     def __validate_body(self,body: Dict) -> List[float]:
       if "numbers" not in body:
-        raise Exception("body mal formatado!")
+        raise HttpUnprocessableEntityError("body mal formatado!")
       
       input_data = body["numbers"]
       return input_data
@@ -39,7 +42,7 @@ class Calculator3:
     
     def __verify_results(self,variance: float,multiplation:float) ->None:
        if variance < multiplation:
-          raise Exception("Falha no processo: Variância menor que / Multiplicação!!")
+          raise HttpBadRequestError("Falha no processo: Variância menor que / Multiplicação!!")
    
    
     def __format_response(self,variance:float)-> Dict:
